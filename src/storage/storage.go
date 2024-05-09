3f1f62ee289dfe00a12935e5d35c72e890aa816e
OnlyFor <OnlyFor@users.noreply.github.com>
2024-05-09 10:30:32 +0800

    Update storage.go

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	"fmt"
 	"database/sql"
 	sqlite3 "github.com/mattn/go-sqlite3"
 )
@@ -12,37 +11,24 @@ type Storage struct {
 
 func New(path string) (*Storage, error) {
 
-	sql.Register("sqlite3_with_extensions",
-		&sqlite3.SQLiteDriver{
-			Extensions: []string{
-				"/usr/local/lib/zstd_vfs.so",
-			},
-		})
+	// sql.Register("sqlite3_with_extensions",
+	// 	&sqlite3.SQLiteDriver{
+	// 		Extensions: []string{
+	// 			"/usr/local/lib/libsqlite_zstd.so",
+	// 		},
+	// 	})
 
-	db, err := sql.Open("sqlite3_with_extensions", ":memory:")
+	// db, err := sql.Open("sqlite3_with_extensions", path)
+	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
 	}
 
-    _, err = db.Exec("SELECT load_extension('/usr/local/lib/zstd_vfs.so')")
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = db.Exec("PRAGMA writable_schema = 1")
-	if err != nil {
-		return nil, err
-	}
-
-    _, err = db.Exec(fmt.Sprintf("ATTACH DATABASE '%s' ", path))
-	if err != nil {
-		return nil, err
-	}
 	// TODO: https://foxcpp.dev/articles/the-right-way-to-use-go-sqlite3
 	db.SetMaxOpenConns(1)
 
-	// if err = migrate(db); err != nil {
-	// 	return nil, err
-	// }
+	if err = migrate(db); err != nil {
+		return nil, err
+	}
 	return &Storage{db: db}, nil
 }
