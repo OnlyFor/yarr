7cde3030c4887a4e99b013c84a957c07b4202d3d
OnlyFor <OnlyFor@users.noreply.github.com>
2024-05-09 09:51:03 +0800

    Update storage.go

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -30,8 +30,8 @@ func New(path string) (*Storage, error) {
 	// TODO: https://foxcpp.dev/articles/the-right-way-to-use-go-sqlite3
 	db.SetMaxOpenConns(1)
 
-	if err = migrate(db); err != nil {
-		return nil, err
-	}
+	// if err = migrate(db); err != nil {
+	// 	return nil, err
+	// }
 	return &Storage{db: db}, nil
 }
